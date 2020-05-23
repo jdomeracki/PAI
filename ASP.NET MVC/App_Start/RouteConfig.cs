1a7cf9616d7bbb7b3f38f8378e6be6b5f255220e
jdomeracki <j.domeracki96@gmail.com>
2020-05-23 18:47:11 +0200

    Tutorial finished

---
+++ ASP.NET MVC/App_Start/RouteConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ASP.NET_MVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            /*routes.MapRoute(
+               name: "Songs",
+               url: "songs/square/{id}",
+               defaults: new { controller = "Songs", action = "Action", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+              name: "23",
+              url: "",
+              defaults: new { controller = "Songs", action = "Action", id = 23 }
+          );*/
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Songs", action = "Index", id = UrlParameter.Optional }
+            );
+
+        }
+    }
+}
